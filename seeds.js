1598d6aa88548b0c0071adc7072e93cfe9c1ed14
Suzanne <suzanne.aitchison@gmail.com>
2017-04-18 19:49:51 +0100

    db seeded

---
+++ seeds.js
@@ -0,0 +1,23 @@
+use arrg_players;
+
+db.players.insert([
+{
+  name: "CrazyLegs",
+  number: 11,
+  positions: ["Blocker", "Pivot"],
+  team: "All Stars"
+},
+{
+  name: "So Solid Sue",
+  number: 251,
+  positions: ["Jammer", "Blocker"]
+},
+{
+  name: "Tenacious C",
+  number: 269,
+  positions: ["Jammer", "Blocker"],
+  team: "All Star Rookies"
+}
+])
+
+
